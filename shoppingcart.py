6593bb9af73be07bc70effd9048aeafdc9c879c2
kgarth <kade.garth@csuglobal.edu>
2025-07-18 15:33:33 -0700

    shoppingcart.py completed

---
+++ shoppingcart.py
@@ -0,0 +1,75 @@
+class ShoppingCart():
+    customer_name = ''
+    current_date = ''
+    cart_items = []
+
+    def __init__(self):
+        self.customer_name = 'none'
+        self.current_date = 'January 1, 2020'
+
+    def set_name(self, name):
+        self.customer_name = name
+
+    def set_date(self, date):
+        self.current_date = date
+
+    def add_item(self, ItemToPurchase):
+        for item in self.cart_items:
+            if item['Name'] == ItemToPurchase['Name']:
+                print('Item already in cart')
+        else:
+            self.cart_items.append(ItemToPurchase)
+
+    def remove_item(self, ItemToRemove):
+        for pos, item in enumerate(self.cart_items):
+            if item['Name'] == ItemToRemove:
+                del self.cart_items[pos]
+        else:
+            print('Item not found in cart. Nothing removed.')
+
+    def modify_item(self, ItemToPurchase):
+        for pos, item in enumerate(self.cart_items):
+            if item['Name'] == ItemToPurchase['Name']:
+                if item['Quantity'] != ItemToPurchase['Quantity']:
+                    self.cart_items[pos]['Quantity'] = ItemToPurchase['Quantity']
+                if item['Cost'] != ItemToPurchase['Cost']:
+                    self.cart_items[pos]['Cost'] = ItemToPurchase['Cost']
+                if item['Description'] != ItemToPurchase['Description']:
+                    self.cart_items[pos]['Description'] = ItemToPurchase['Description']
+                break
+        else:
+            print('Item not found in cart. Nothing modified')
+
+    def get_num_items_in_cart(self):
+        return len(self.cart_items)
+
+    def get_cost_of_cart(self):
+        total = 0
+        for item in self.cart_items:
+            total += (item['Cost'] * item['Quantity'])
+        return total
+
+    def print_toal(self):
+        if len(self.cart_items) == 0:
+            print('SHOPPING CART IS EMTPY')
+        else:
+            print('{}\'s Shopping Cart - {}'.format(self.customer_name, self.current_date))
+            print('Number of Items: {}'.format(self.get_num_items_in_cart()))
+            
+            for item in self.cart_items:
+                print('{} {} @ ${} = ${}'.format(item['Name'], item['Quantity'],
+                                                 item['Cost'], (item['Cost'] * 
+                                                                item['Quantity'])))
+
+            print('Total: {}'.format(self.get_cost_of_cart()))
+
+    def print_descriptions(self):
+        if len(self.cart_items) == 0:
+            print('SHOPPING CART IS EMTPY')
+        else:
+            print('{}\'s Shopping Cart - {}'.format(self.customer_name, self.current_date))
+            print('Item Descriptions')
+
+            for item in self.cart_items:
+                print('{}: {}'.format(item['Name'], item['Description']))
+
